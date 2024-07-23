abbbb1832ae315da60eb04be4ddd2fd656f8d8a5
Jutamat1105 <653070310202@npu.ac.th>
2024-07-23 16:05:43 +0700

    new contract

---
+++ tips.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract tips {
+    address owner;
+    constructor(){
+        owner = msg.sender;
+    }
+
+    //1.หยอดเงินเข้าไปในกระปุก
+
+    //2.เช็คเงินในกระปุก
+
+    //3.เพิ่มคนใช้งาน
+
+    //4.เอาคนใช้งานออก
+
+    //5.ดูคนใช้งาน
+
+    //6.disribute tips
+    
+}
