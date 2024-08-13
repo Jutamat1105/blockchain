29e1febbc9c3b0833d3965720da869c499d3448d
Jutamat1105 <653070310202@npu.ac.th>
2024-08-13 15:27:58 +0700

    add waitress

--- tips.sol
+++ tips.sol
@@ -3,6 +3,11 @@ pragma solidity 0.8.26;
 
 contract tips {
     address owner;
+
+    Waitress[] waitress;
+
+
+
     constructor(){
         owner = msg.sender;
     }
@@ -19,8 +24,37 @@ contract tips {
         return address(this).balance;
     }
 
-    //3. add User
+    //3.1 add structure waitress
+    struct Waitress{
+        address payable walletAddress;
+        string name;
+    }
+
+    //3.2 add waitress
+    function addWaitress(address payable walletAddress,string memory name) public {
+    bool waitressExist = false;
+
+    if (waitress.length >=1){
+    for (uint i=0; i<waitress.length; i++){
+            if( waitress[i].walletAddress == walletAddress){
+                waitressExist = true;
+            }
+            }
+
+        }
+        if(waitressExist==false){
+            waitress.push(Waitress(walletAddress,name));
+        }
+
+    }
+
+    //4.remove User
+
+    //5. view User
+    function viewWaitress() public  view returns  (Waitress[] memory) {
+        return waitress;
+    }
     
-   
+   //6. distribute tips
 
    }
